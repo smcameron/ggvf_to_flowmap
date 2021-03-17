bc6fd1c123a668ad83f947585de9c8e2b5d96730
Stephen M. Cameron <stephenmcameron@gmail.com>
2021-03-17 19:56:45 -0400

    More progress
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- ggvf_to_flowmap.c
+++ ggvf_to_flowmap.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+static char *velocity_field_file;
+
+static struct option long_options[] = {
+	{ "velocity-field",     required_argument, 0,  'v' },
+	{ 0, 0, 0, 0 },
+};
 
 int main(int argc, char *argv[])
 {
+
+	while (1) {
+
+		int option_index = 0;
+		int rc;
+
+		rc = getopt_long(argc, argv, "v:", long_options, &option_index);
+		if (rc == -1)
+			break;
+
+		switch (rc) {
+		case 'v':
+			velocity_field_file = optarg;
+			break;
+		}
+	}
+
 	return 0;	
 }
 
